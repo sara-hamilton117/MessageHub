8b4b2c26e51b49d7ada5c8c5609d877177f6e2ca
sara-hamilton117 <79106622+sara-hamilton117@users.noreply.github.com>
2021-07-28 12:24:43 +0200

    Update connection.php
    Added creds for heroku

--- connection.php
+++ connection.php
@@ -1,9 +1,18 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "messagehub";
+// Local host credentials
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "";
+// $dbname = "messagehub";
+
+// Heroku credentials
+$dbhost = "eu-cdbr-west-01.cleardb.com";
+$dbuser = "b4a74350668e0f";
+$dbpass = "c5d28f87";
+$dbname = "heroku_64958899b23da1c";
+
+
 
 if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname)) 
 {
