6497d8a1ad390379b4d00ab0ea56af5becb0f72d
sara-hamilton117 <79106622+sara-hamilton117@users.noreply.github.com>
2021-07-30 23:33:38 +0200

    last one

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php
 // Local host credentials
-// $dbhost = "localhost";
-// $dbuser = "root";
-// $dbpass = "";
-// $dbname = "messagehub";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "messagehub";
 
 // Heroku credentials
-$dbhost = "eu-cdbr-west-01.cleardb.com";
-$dbuser = "b4a74350668e0f";
-$dbpass = "c5d28f87";
-$dbname = "heroku_64958899b23da1c";
+// $dbhost = "eu-cdbr-west-01.cleardb.com";
+// $dbuser = "b4a74350668e0f";
+// $dbpass = "c5d28f87";
+// $dbname = "heroku_64958899b23da1c";
 
 
 if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname)) 
--- current-services-tab.php
+++ current-services-tab.php
@@ -6,13 +6,13 @@ global $con;
 
 $id = $_SESSION['user_id'];
 
-$sql = "SELECT service.service_img, service.service_address FROM service INNER JOIN serviceuser ON service.service_id = serviceuser.service_id WHERE serviceuser.user_id = '$id'";
+$sql = "SELECT service.service_img, service.service_address, service.service_id FROM service INNER JOIN serviceuser ON service.service_id = serviceuser.service_id WHERE serviceuser.user_id = '$id'";
 $result = $con->query($sql);
 $nodes = array();
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        array_push($nodes, '<div class="bd-highlight d-flex align-items-center row tab" onclick="opensite(\''. $row["service_address"].'\')"><img class="svg p-0" src='. $row["service_img"] .'></div>');
+        array_push($nodes, '<div class="bd-highlight d-flex align-items-center row tab" id="left-service-' . $row["service_id"] . '" onclick="opensite(\''. $row["service_address"].'\')"><img class="svg p-0" src='. $row["service_img"] .'></div>');
     }
     echo json_encode($nodes);
 } else {
